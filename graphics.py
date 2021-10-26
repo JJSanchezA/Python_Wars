67ec96d41c306fd196539973f1f9dc8a494ea5d2
Juan José Sánchez Á <jjsanchez@protonmail.com>
2021-10-26 12:09:05 +0200

    Creado archivo graficos graphics.py

---
+++ graphics.py
@@ -0,0 +1,66 @@
+# Listados de archivos gráficos usados para el juego.
+
+# Gráfico de Ties
+grf_tief = 'data/tie.png'
+# Gráfico del shuttle
+grf_shuttle = 'data/shuttle.png'
+# Gráfico del transporte de troopers
+grf_stroopers = 'data/stroopers.png'
+# Gráfico del M.Falcon
+grf_falconm = 'data/MFalcon.png'
+# Gráfico de una Corveta
+grf_corveta = 'data/corveta.png'
+# Gráfico de los disparos verdes
+grf_laser_verde = 'data/laser_ve.png'
+# Gráfico de los disparos rojos
+grf_laser_rojo = 'data/laser_ro.png'
+# Gráfico del fondo de estrellas
+grf_fondo = "data/stars_bg.png"
+# Lista de animación de xwing
+grf_lst_xwing = ['data/xwing0000.png',
+                'data/xwing0001.png',
+                'data/xwing0002.png',
+                'data/xwing0003.png',
+                'data/xwing0004.png',
+                'data/xwing0005.png',
+                'data/xwing0006.png',
+                'data/xwing0007.png',
+                'data/xwing0008.png',
+                'data/xwing0009.png',
+                'data/xwing0010.png',
+                'data/xwing0011.png',
+                'data/xwing0012.png',
+                'data/xwing0013.png',
+                'data/xwing0014.png',
+                'data/xwing0015.png',
+                'data/xwing0016.png',
+                'data/xwing0017.png',
+                'data/xwing0018.png',
+                'data/xwing0019.png',
+                'data/xwing0020.png',
+                'data/xwing0021.png',
+                'data/xwing0022.png',
+                'data/xwing0023.png',
+                'data/xwing0024.png',
+                'data/xwing0025.png'
+                'data/xwing0026.png',
+                'data/xwing0027.png',
+                'data/xwing0028.png',
+                'data/xwing0029.png',
+                'data/xwing0030.png',
+                ]
+# Lista de gráficos para la secuencia de explosión.
+grf_lst_explosion = ["data/exp_01.png",
+                   "data/exp_02.png",
+                   "data/exp_03.png",
+                   "data/exp_04.png",
+                   "data/exp_05.png",
+                   "data/exp_06.png",
+                   "data/exp_07.png",
+                   "data/exp_08.png",
+                   "data/exp_09.png",
+                   "data/exp_10.png",
+                   "data/exp_11.png",
+                   "data/exp_12.png",
+                   "data/exp_13.png",
+                   ]
